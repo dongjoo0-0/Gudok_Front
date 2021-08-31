bbb04adcf588d24ad7d8997e9a40416bc73f7b12
dongjoo0-0 <kdj486347@gmail.com>
2021-08-31 20:32:21 +0900

    Render Main Components and Add library to use icons

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import Topbar from './Topbar';
+import ArticleBoard from './ArticleBoard';
+import Navbar from './Navbar';
+//import Sidebar from './Sidebar';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faBars, faArrowLeft, faHome, faUserCircle, faSearch } from '@fortawesome/free-solid-svg-icons'
+
+library.add( fab, faBars, faArrowLeft, faHome, faUserCircle, faSearch );
 
 export default function App() {
+  const [openSidebar, setOpenSidebar] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.statusBar} />
+      <Topbar />
+      <ArticleBoard />
+      <Navbar />
       <StatusBar style="auto" />
     </View>
+    /* 
+    login board
+    register board
+    top title bar / article board / bot nav bar / side bar -> main page
+    top search bar / search autocomplete board
+    top result bar
+    reserved word bar
+    profile / menu list
+    */
   );
 }
 
@@ -18,4 +42,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  statusBar: {
+    height: Constants.statusBarHeight,
+  }
 });
