3fc927fbbed02d0ed14d7b04373a093393d0e15e
dongjoo0-0 <kdj486347@gmail.com>
2021-09-02 23:40:00 +0900

    Add Home screen to application with Top/Main/Bot bar

---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Constants from 'expo-constants';
+import Topbar from '../components/Topbar';
+import ArticleBoard from '../components/ArticleBoard';
+import Navbar from '../components/Navbar';
+//import Sidebar from '../components/Sidebar';
+
+export default HomeScreen = ({ navigation }) => {
+  return(
+    <View style={styles.container}>
+      <View style={styles.statusBar} />
+      <Topbar />
+      <ArticleBoard />
+      <Navbar />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  statusBar: {
+    height: Constants.statusBarHeight,
+  }
+});
