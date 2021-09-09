f3994ed0e198bc9a5f378bd899d85b661763baa5
dongjoo0-0 <kdj486347@gmail.com>
2021-09-09 22:24:58 +0900

    Design Register Screen only text

---
+++ app/screens/RegisterScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default RegisterScreen = ({ navigation }) => {
+  return(
+    <View style={styles.container}>
+      <Text>Hello, Register</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
