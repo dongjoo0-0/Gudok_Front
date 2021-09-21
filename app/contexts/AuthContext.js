3303c317e0a13e3c6c173e1445528edcd9a3c424
dongjoo0-0 <kdj486347@gmail.com>
2021-09-21 17:01:24 +0900

    Revise typo in function parameter

--- app/contexts/AuthContext.js
+++ app/contexts/AuthContext.js
@@ -4,7 +4,7 @@ import React, { useState, createContext } from 'react';
 export const AuthContext = createContext();
 
 // Context를 제공하는 Provider를 export한다. useState를 통해 얻은 authState, setAuthState를 children에서 사용할 수 있게 된다.
-export const AuthProvider = ({props}) => {
+export const AuthProvider = (props) => {
   const [authState, setAuthState] = useState({
     _id: '',
     email: '',
