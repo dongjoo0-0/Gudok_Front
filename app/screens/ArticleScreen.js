64339b7415a475bc5a957f27b37e5ef48cbccc6b
dongjoo0-0 <kdj486347@gmail.com>
2021-09-21 17:02:30 +0900

    Add WebView in article screen

--- app/screens/ArticleScreen.js
+++ app/screens/ArticleScreen.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 // 부모에서 props를 통해 article data를 받아오고 렌더링 해주면 될듯.
 export default ArticleScreen = ({ navigation }) => {
   return(
-    <View style={styles.container}>
-      <Text>Hello, Article</Text>
-    </View>
+    <WebView 
+      source={{ uri: 'https://m.naver.com' }}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
