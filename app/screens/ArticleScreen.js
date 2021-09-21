022866d3b23fb3ea7ed74143ee8070a5c56cff4d
dongjoo0-0 <kdj486347@gmail.com>
2021-09-21 21:17:49 +0900

    Add links with webview in the article

--- app/screens/ArticleScreen.js
+++ app/screens/ArticleScreen.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
 
-// 부모에서 props를 통해 article data를 받아오고 렌더링 해주면 될듯.
-export default ArticleScreen = ({ navigation }) => {
+export default ArticleScreen = ({ navigation, route }) => {
   return(
     <WebView 
-      source={{ uri: 'https://m.naver.com' }}
+      source={{ uri: route.params.uri }}
     />
   );
 };
