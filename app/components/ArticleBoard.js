09207b53bbfa03e9a63b39b1d0f31cf3b3079d92
dongjoo0-0 <kdj486347@gmail.com>
2021-09-23 00:43:10 +0900

    Add logging getPosts finished successfully or finished with error.

--- app/components/ArticleBoard.js
+++ app/components/ArticleBoard.js
@@ -17,8 +17,10 @@ export default ArticleBoard = ({ navigation }) => {
       .get("http://172.30.1.47:5000/api/post?limit=10")
       .then(response => {
         setPosts(response.data.posts.map(item => convertIdToString(item)));
+        console.log("Finished loading posts");
       })
       .catch(error => {
+        console.log(error);
         alert(error.message);
       });
   }, []);
