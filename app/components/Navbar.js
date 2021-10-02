25a4e55a2bfbc5a152749f91ad2311c9639f15c8
dongjoo0-0 <kdj486347@gmail.com>
2021-10-02 19:38:55 +0900

    Fix: Add semicolon in module import

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 
 // Bottom bar를 나타낸다. 사실 BottomTapNavigator가 존재하므로 글 / 로그인 로직 처리 이후 HomeScreen을 BottomTapNavigator로 만들려고 한다. 그러려면 이건 사라질 운명..
 export default function Navbar() {
--- app/components/Topbar.js
+++ app/components/Topbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 
 // 이름은 Topbar인데 있는건 메뉴(사이드바)가 다다. 사실 원래는 custom header로 쓰려 했으나 디자인 요소라 생각해서 미뤄둠.
 export default Topbar = () => {
