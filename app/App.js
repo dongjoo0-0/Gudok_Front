f4a6fd6d23dc85f492a4fcc1b1bcdc744501f2f3
dongjoo0-0 <kdj486347@gmail.com>
2021-11-06 22:28:36 +0900

    Refactor: Stack navigator moved app navigate btw auth-content

--- app/App.js
+++ app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import SplashScreen from './screens/SplashScreen';
 
@@ -16,11 +17,62 @@ import { faBars, faArrowLeft, faHome, faUserCircle, faSearch } from '@fortawesom
 
 library.add( fab, faBars, faArrowLeft, faHome, faUserCircle, faSearch );
 
+const Stack = createNativeStackNavigator();
+
+const Auth = () => {
+  return(
+    <Stack.Navigator initialRouteName="LogIn">
+      <Stack.Screen 
+        name="LogIn" 
+        component={ LogInScreen } 
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen 
+        name="Register" 
+        component={ RegisterScreen }
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+const Content = () => {
+  return(
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen 
+        name="Home" 
+        component={ HomeScreen } 
+        options={{ headerLeft: () => <Topbar/>, headerTitleAlign: 'center' }}
+      />
+      <Stack.Screen 
+        name="Article" 
+        component={ ArticleScreen }
+      />
+    </Stack.Navigator>
+  );
+}
+
 export default App = () => {
   return (
     <AuthProvider>
       <NavigationContainer>
-        <SplashScreen />
+        <Stack.Navigator initialRouteName="Splash">
+          <Stack.Screen 
+            name="Splash"
+            component={SplashScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Auth"
+            component={Auth}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Content"
+            component={Content}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
         <StatusBar style="auto" />
       </NavigationContainer>
     </AuthProvider>
