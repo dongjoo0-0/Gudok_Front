e84b7e9fde2c818acdb6c980b0fa7a52f5c051eb
dongjoo0-0 <kdj486347@gmail.com>
2021-11-13 21:46:37 +0900

    Feat: send register inform. to server

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,10 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, TextInput, ScrollView } from 'react-native';
 
 export default RegisterScreen = ({ navigation }) => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [checkPwd, setCheckPwd] = useState("");
+  const [pwdErr, setPwdErr] = useState("");
+
+  useEffect(() => {
+    if(password != checkPwd){
+      setPwdErr("비밀번호가 일치하지 않습니다.");
+    }
+    else if (password == checkPwd){
+      setPwdErr("");
+    }
+  }, [checkPwd]);
+
+  const handleSubmit = () => {
+    if(!email) {
+      alert("이메일을 입력하지 않았습니다.");
+      return;
+    }
+    if(!password) {
+      alert("비밀번호를 입력하지 않았습니다.");
+      return;
+    }
+    if(password != checkPwd){
+      alert("비밀번호가 일치하지 않습니다.");
+      return;
+    }
+    let form = {"user": {"email": email, "password": password}};
+    /*axios.post("http://localhost:5000/api/user/login", form)
+      .then(response => {
+        console.log(response);
+        //AsyncStorage.setItem('user', response.data.user);
+        navigation.replace("SplashScreen");
+      })
+      .catch(error => {
+        console.log(error);
+        alert(error.message);
+      })*/
+    navigation.replace("LogIn");
+  }
 
   return(
     <ScrollView contentContainerStyle={styles.container}>
@@ -12,18 +49,21 @@ export default RegisterScreen = ({ navigation }) => {
       <Text style={styles.title}>Register</Text>
 
       <View style={styles.items}>
-        <Text>Username</Text>
-        <TextInput 
-          style={styles.input}
-          onChangeText={setUsername}
-          text={username}
-          autoCompleteType={'username'}
-          textContentType={'username'}
-          returnKeyType={'next'}
-        />
-        <TouchableOpacity style={styles.button}>
-          <Text style={{color: 'white'}}>중복 확인</Text>
-        </TouchableOpacity>
+        <Text>Email</Text>
+        <View style={{flexDirection: 'row'}}>
+          <TextInput 
+            style={[styles.input, styles.email]}
+            onChangeText={setEmail}
+            text={email}
+            keyboardType={'email-address'}
+            autoComplete={'email'}
+            textContentType={'emailAddress'}
+            returnKeyType={'next'}
+          />
+          <TouchableOpacity style={[styles.button, styles.emailBtn]}>
+            <Text style={{color: 'white'}}>중복 확인</Text>
+          </TouchableOpacity>
+        </View>
         <Text>Password</Text>
         <TextInput 
           style={styles.input}
@@ -44,9 +84,12 @@ export default RegisterScreen = ({ navigation }) => {
           textContentType={'password'}
           returnKeyType={'done'}
         />
+        <Text style={pwdErr != '' ? styles.pwdErrStyle: styles.opacity}>
+          {pwdErr}
+        </Text>
       </View>
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
         <Text style={{color: 'white'}}>Register</Text>
       </TouchableOpacity>
     </ScrollView>
@@ -68,7 +111,8 @@ const styles = StyleSheet.create({
   },
   items: {
     padding: 30,
-    height: 400,
+    paddingBottom: 15,
+    height: 300,
     justifyContent: 'space-between',
   },
   button : {
@@ -84,5 +128,21 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     paddingHorizontal: 10,
     paddingVertical: 3,
+  },
+  email: {
+    width: '70%',
+    marginRight: 5,
+  },
+  emailBtn: {
+    alignSelf: 'auto'
+  },
+  pwdErrStyle: {
+    color: 'red',
+    fontSize: 12,
+    opacity: 1,
+  },
+  opacity: {
+    fontSize: 12,
+    opacity: 0,
   }
 });
